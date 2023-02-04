29b1142ce1b2de4e9ceab7276e62b24bee5becd5
Sepran Ashari <sepran@jogjamedia.co.id>
2023-02-04 15:43:47 +0700

    First commit to initiate project

---
+++ example/flutter_backblaze_example.dart
@@ -0,0 +1,6 @@
+import 'package:flutter_backblaze/flutter_backblaze.dart';
+
+void main() {
+  var awesome = Awesome();
+  print('awesome: ${awesome.isAwesome}');
+}
---
+++ test/flutter_backblaze_test.dart
@@ -0,0 +1,16 @@
+import 'package:flutter_backblaze/flutter_backblaze.dart';
+import 'package:test/test.dart';
+
+void main() {
+  group('A group of tests', () {
+    final awesome = Awesome();
+
+    setUp(() {
+      // Additional setup goes here.
+    });
+
+    test('First Test', () {
+      expect(awesome.isAwesome, isTrue);
+    });
+  });
+}
